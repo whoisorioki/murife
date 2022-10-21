77741ebe449b93110aedce01b84ca7af32b8b260
Adrian Orioki <adrianorioki@gmail.com>
2022-10-21 08:52:05 +0300

    Apps

--- murife/hello/views.py
+++ murife/hello/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
